d20af83280d8813d8eba36fe7194e1c1cc147a14
Amanda Moraes <amandascm23@gmail.com>
2022-04-14 18:11:37 -0300

    Refact circles generation

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
-const generate_circles_positions = (dimensions, layers=6, circles_per_layer=14, circle_radius=8) => {
+const generate_circles_positions = (dimensions, layers=6, circles_per_layer=14, circle_radius=8, total_circles=81) => {
     const [x, y, w, h] = dimensions
     const center = {'x': (x + w/2),
                     'y': (y + h - circle_radius)}
     const layers_gap = 1 * circle_radius
     const min_radius = (w - (2 * layers * 2 * circle_radius) - (2 * (layers - 1) * layers_gap)) / 2
+    const gap_angle = Math.PI / (circles_per_layer - 1)
 
     const circles = []
     // iterate over desired layers
-    for(let i=1; i<=layers; i++) {
-        // current layer radius = initial radius + past layers circle diameter + past gaps + current layer circle radius
-        const radius = min_radius + ((i-1) * 2 * circle_radius) + ((i-1) * layers_gap) + circle_radius
-
-        for(let c=0; c<circles_per_layer; c++) {
+    for(let c=0; c<circles_per_layer; c++) {
+        const circle_angle = c * gap_angle
+        for(let l=1; l<=layers; l++) {
+            const layer_radius = min_radius + ((l-1) * 2 * circle_radius) + ((l-1) * layers_gap) + circle_radius
             const circle={} // cx, cy, r
-            const gap_angle = Math.PI / (circles_per_layer - 1)
-            const circle_angle = c * gap_angle
-            const cy =  center.y - Math.sin(circle_angle) * radius
-            const cx = center.x + Math.cos(circle_angle) * radius
+            const cy =  center.y - Math.sin(circle_angle) * layer_radius
+            const cx = center.x + Math.cos(circle_angle) * layer_radius
             circle['cx'] = cx
             circle['cy'] = cy
             circle['r'] = circle_radius
@@ -45,6 +43,14 @@ const main = () => {
         .attr("cy", d => d.cy)
         .attr("r", d => d.r)
         .style('fill', 'gray')
+    
+    svg.selectAll('text')
+        .data(circles)
+        .enter()
+        .append('text')
+        .attr('x', d => d.cx)
+        .attr('y', d => d.cy)
+        .text((d,i)=> i)
 }
 
 main()
